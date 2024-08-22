85e2b425fc8d600b958090b92e8b5f8810614184
Esther Toyin <amooesther2503@gmail.com>
2024-08-22 17:24:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const thanks = document.getElementById('cardContainer');
+const thankYou = document.getElementById('thankYouWrapper');
+
+const submitFeedback = () => {
+    thanks.style.display = "none";
+    thankYou.style.display = "flex";
+}
